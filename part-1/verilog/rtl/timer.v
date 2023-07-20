a127ba393061988d063647f92ba94d9de1eddf0f
mo-hosni <m.hosni.ibra@gmail.com>
2023-07-20 12:49:48 +0300

    add part-1 RTL files

---
+++ part-1/verilog/rtl/timer.v
@@ -0,0 +1,160 @@
+/*
+    A simple user's project example - Part 1
+    Minutes and Seconds clock. The design drives 4 multiplexed 7-Segment display digits.
+    It uses Caravel system clock and reset and 11 I/O pads. 
+    The 11 I/O pads are divided between 4 digit enables and 7 segment drivers.
+    The RTL supports:
+     - Common Anode (CC=0) or Common Cathode (CC=1) multiplexed display
+     - Different clock frequencies (FREQ in Hertz)
+     - Different scans by sec (SCAN_PER_SEC)
+*/
+
+module timer #(parameter  CC = 1,
+                                FREQ = 2_000,
+                                SCAN_PER_SEC = 25) 
+(
+    input   wire        clk,
+    input   wire        rst,
+    output  wire [7:0]  seven_seg,
+    output  wire [3:0]  digit_en     
+);
+    localparam DIG_DURATION = (FREQ)/(4 * SCAN_PER_SEC);
+
+    // the counter
+    reg [3:0]   sec_ones, sec_tens;
+    reg [3:0]   min_ones, min_tens;
+    wire        nine_sec        = (sec_ones == 4'd9);
+    wire        fifty_nine_sec  = (sec_tens == 4'd5) & nine_sec;
+    wire        nine_min        = (min_ones == 4'd9);
+    wire        fifty_nine_min  = (min_tens == 4'd5) & nine_min;
+    
+    // Time Base Generators
+    reg         sec;
+    reg         scan;
+
+    reg [31:0]  sec_div;
+    reg [31:0]  scan_div;
+    
+    always @(posedge clk or posedge rst) begin
+        if(rst)
+            sec_div <= 32'b0;
+        else if(sec_div == FREQ)
+            sec_div <= 32'b0;
+        else
+            sec_div <= sec_div + 32'b1;
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if(rst)
+            scan_div <= 32'b0;
+        else if(scan_div == DIG_DURATION)
+            scan_div <= 32'b0;
+        else
+            scan_div <= scan_div + 32'b1;
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if(rst)
+            sec <= 1'b0;
+        else if(sec_div == FREQ)
+            sec <= 1'b1;
+        else 
+            sec <= 1'b0;
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if(rst)
+            scan <= 1'b0;
+        else if(scan_div == DIG_DURATION)
+            scan <= 1'b1;
+        else 
+            scan <= 1'b0;
+    end
+
+
+    // Seconds Counters
+    always @(posedge clk or posedge rst)
+        if(rst) 
+            sec_ones <=  4'b0;
+        else if(sec) begin
+            if(!nine_sec) 
+                sec_ones <= sec_ones + 4'd1;
+            else 
+                sec_ones <= 0;
+        end
+
+    always @(posedge clk or posedge rst)
+        if(rst) 
+            sec_tens <=  4'b0;
+        else if(sec) begin 
+            if(fifty_nine_sec) 
+                sec_tens <= 0;
+            else if(nine_sec) 
+                sec_tens <= sec_tens + 4'd1;
+        end
+
+    // Minutes Counters
+    always @(posedge clk or posedge rst)
+        if(rst) 
+            min_ones <=  4'b0;
+        else if(fifty_nine_sec & sec) begin
+            if(!nine_min)
+                min_ones <= min_ones + 4'd1;
+            else
+                min_ones <= 0;
+        end
+
+    always @(posedge clk or posedge rst)
+        if(rst) 
+            min_tens <=  4'b0;
+        else if(fifty_nine_sec & sec) begin
+            if(fifty_nine_min) 
+                min_tens <= 0;
+            else if(nine_min) 
+                min_tens <= min_tens + 4'd1;
+        end
+    
+    
+        // Display TDM
+        reg [1:0] dig_cnt;
+        always @(posedge clk or posedge rst) begin
+            if(rst)
+                dig_cnt <= 2'b0;
+            else
+                if(scan) dig_cnt <= dig_cnt + 1'b1;
+        end
+
+        wire [3:0]  bcd_mux =   (dig_cnt == 2'b00) ? sec_ones :
+                                (dig_cnt == 2'b01) ? sec_tens :
+                                (dig_cnt == 2'b10) ? min_ones : min_tens;
+
+        // BCD to 7SEG Decoder
+        reg [7:0]   ca_7seg;
+        wire[7:0]   cc_7seg = ~ ca_7seg;
+        always @* begin
+            ca_7seg = 7'b0000000;
+            case(bcd_mux)
+                4'd0 : ca_7seg = 7'b0000001;
+                4'd1 : ca_7seg = 7'b1001111;
+                4'd2 : ca_7seg = 7'b0010010;
+                4'd3 : ca_7seg = 7'b0000110;
+                4'd4 : ca_7seg = 7'b1001100;
+                4'd5 : ca_7seg = 7'b0100100;
+                4'd6 : ca_7seg = 7'b0100000;
+                4'd7 : ca_7seg = 7'b0001111;
+                4'd8 : ca_7seg = 7'b0000000;
+                4'd9 : ca_7seg = 7'b0000100;
+            endcase
+        end
+
+        generate
+            if(CC==0) begin
+                assign seven_seg    =   ca_7seg;
+                assign digit_en     =   (4'b1 << dig_cnt);
+            end else begin
+                assign seven_seg    =   cc_7seg;
+                assign digit_en     =   ~(4'b1 << dig_cnt);
+            end
+        endgenerate
+
+endmodule
---
+++ part-1/verilog/rtl/user_proj_timer.v
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_timer #(
+    parameter BITS = 16
+)(
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wb_clk_i,
+    input wb_rst_i,
+
+    // IOs
+    input  [11:0] io_in,
+    output [11:0] io_out,
+    output [11:0] io_oeb
+);
+
+    wire [7:0] seven_seg;
+    wire [3:0] digit_en;
+
+    timer timer(
+        .clk(wb_clk_i),
+        .rst(wb_rst_i),
+        .seven_seg(seven_seg),
+        .digit_en(digit_en)
+    );
+
+    assign io_out = {seven_seg, digit_en};
+    assign io_oeb = 12'd0;
+    
+endmodule
+
+`default_nettype wire
